501e9eff0f2fed2d30667aa7af44ccd6107ae659
Milan Sony <milanfrom2001@gmail.com>
2022-08-11 02:19:37 +0530

    Book Online Section Created

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function readMore(){
+// Read More Button
+function readMore() {
   let dots = document.getElementById("dots");
   let moreText = document.getElementById("more-text");
   let readMoreBtn = document.getElementById("read-more-btn");
-  // 1st Check if the dots are visible or not. 
-  if(dots.style.display!="none"){
+  // 1st Check if the dots are visible or not.
+  if (dots.style.display != "none") {
     //If the dots are not visible (dots not equal to null/none), we are going to hide that dots.
-    dots.style.display="none";
+    dots.style.display = "none";
     // Display More Text
-    moreText.style.display="inline";
+    moreText.style.display = "inline";
     // Change Btn name
-    readMoreBtn.innerHTML="Read Less";
-  }
-  else{
-    dots.style.display="inline";
-    moreText.style.display="none";
-    readMoreBtn.innerHTML="Read More";
+    readMoreBtn.innerHTML = "Read Less";
+  } else {
+    dots.style.display = "inline";
+    moreText.style.display = "none";
+    readMoreBtn.innerHTML = "Read More";
   }
 }
